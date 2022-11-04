86cb0f2f6fef83533409fea8a04009dfa3b82893
Andrew <atrybushnyi@kse.org.ua>
2022-11-04 08:45:02 +0200

    Analyze diff

--- Program.cs
+++ Program.cs
@@ -2,73 +2,78 @@
 var letterFrequencyDict = new SortedDictionary<char, int>();
 foreach (var c in text.Where(c => !char.IsPunctuation(c) && c != '\0' && c != ' '))
 {
-    if (!letterFrequencyDict.ContainsKey(c))
-    {
-        letterFrequencyDict.Add(c, 1);
-    }
-    else
-    {
-        letterFrequencyDict[c]++;
-    }
+	if (!letterFrequencyDict.ContainsKey(c))
+	{
+		letterFrequencyDict.Add(c, 1);
+	}
+	else
+	{
+		letterFrequencyDict[c]++;
+	}
 }
 
 Dictionary<char, double> toCompare = new Dictionary<char, double>
 {
-    {'e', 11.1607},
-    {'a', 8.4966},
-    {'r', 7.5809},
-    {'i', 7.5448},
-    {'o', 7.1635},
-    {'t', 6.9509},
-    {'n', 6.6544},
-    {'s', 5.7351},
-    {'l', 5.4893},
-    {'c', 4.5388},
-    {'u', 3.6308},
-    {'d', 3.3844},
-    {'p', 3.3844},
-    {'m', 3.0129},
-    {'h', 3.0034},
-    {'g', 2.4705},
-    {'b', 2.0720},
-    {'f', 1.8121},
-    {'y', 1.7779},
-    {'w', 1.2899},
-    {'k', 1.1016},
-    {'v', 1.0074},
-    {'x', 0.2902},
-    {'z', 0.2722},
-    {'j', 0.1965},
-    {'q', 0.1962}
+	{'e', 11.1607},
+	{'a', 8.4966},
+	{'r', 7.5809},
+	{'i', 7.5448},
+	{'o', 7.1635},
+	{'t', 6.9509},
+	{'n', 6.6544},
+	{'s', 5.7351},
+	{'l', 5.4893},
+	{'c', 4.5388},
+	{'u', 3.6308},
+	{'d', 3.3844},
+	{'p', 3.3844},
+	{'m', 3.0129},
+	{'h', 3.0034},
+	{'g', 2.4705},
+	{'b', 2.0720},
+	{'f', 1.8121},
+	{'y', 1.7779},
+	{'w', 1.2899},
+	{'k', 1.1016},
+	{'v', 1.0074},
+	{'x', 0.2902},
+	{'z', 0.2722},
+	{'j', 0.1965},
+	{'q', 0.1962}
 };
 
 Decrypt(3, toCompare);
 
 SortedDictionary<char, int> Decrypt(int key, Dictionary<char, double> letterFrequencyDict)
 {
-    var shiftedLetterFreqDict = new SortedDictionary<char, int>();
-    foreach (var pair in letterFrequencyDict)
-    {
-        const string albet = "abcdefghijklmnopqrstuvwxyz";
-        int i = 0;
-        while (pair.Key != albet[i])
-        {
-            i++;
-        }
+	var shiftedLetterFreqDict = new SortedDictionary<char, int>();
+	foreach (var pair in letterFrequencyDict)
+	{
+		const string albet = "abcdefghijklmnopqrstuvwxyz";
+		int i = 0;
+		while(pair.Key != albet[i]){
+			i++;
+		}
+		key = i - key;
+		while(key < 0){
+			key += 25;
+		}
+		while(key > 25){
+			key -= 25;
+		}
+		shiftedLetterFreqDict.Add(albet[key], (int)pair.Value);
+	}
 
-        key = i - key;
-        while (key < 0)
-        {
-            key += 25;
-        }
+	return shiftedLetterFreqDict;
+}
 
-        while (key > 25)
-        {
-            key -= 25;
-        }
-
-        shiftedLetterFreqDict.Add(albet[key], (int) pair.Value);
-    }
-
-    return shiftedLetterFreqDict;
+int Analyze(SortedDictionary<char, int> dictionary, Dictionary<char, double> toCompare)
+{
+	var diff = 0;
+	foreach (var pair in dictionary)
+	{
+		diff += Math.Abs(pair.Value - (int)Math.Floor(toCompare[pair.Key]));
+	}
+	
+	return diff;
 }
